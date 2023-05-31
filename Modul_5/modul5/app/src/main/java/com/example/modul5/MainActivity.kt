10b78778cd90b7959c33fe07cab3ac93232ef74c
Nisak23 <cnisa23@gmail.com>
2023-05-31 12:15:24 +0700

    Menambahkan modul praktikum

--- Modul_5/modul5/app/src/main/java/com/example/modul5/MainActivity.kt
+++ Modul_5/modul5/app/src/main/java/com/example/modul5/MainActivity.kt
@@ -7,16 +7,27 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.example.modul5.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     lateinit var toggle : ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.home -> replacefragment(HomeFragment(),it.title.toString())
+                R.id.setting -> replacefragment(SettingFragment(),it.title.toString())
 
+                else -> {}
+            }
+            true
+        }
         drawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
